2370acd62be92ed676efa836db1b523ef6d0a79f
Lloyd Reyes <lloyd061302@gmail.com>
2024-01-14 13:50:20 -0700

    revert

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "newsAPI": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,25 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { listNews } from '../graphql/queries';
+import React from 'react';
 
-const NewsCards = () => {
-	const [news, setNews] = useState([]);
-
-	useEffect(() => {
-		fetchNews();
-	}, []);
-
-	const fetchNews = async () => {
-		try {
-			const newsData = await API.graphql(graphqlOperation(listNews));
-			const newsList = newsData.data.listNews.items;
-
-			console.log('news list', newsList);
-			setNews(newsList);
-		} catch (error) {
-			console.log('error on fetching news', error);
-		}
-	};
-};
+function NewsCards() {
+	return <div>NewsCards</div>;
+}
 
 export default NewsCards;
