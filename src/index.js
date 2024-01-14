2e6352a368a6584cc63e3f78f1bed78a04a95901
Lloyd Reyes <lloyd061302@gmail.com>
2024-01-14 11:16:30 -0700

    Restart

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
 
-Amplify.configure(amplifyconfig);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
