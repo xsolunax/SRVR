a4b771b15162c75491ad546dedda3c70108d58a3
Lloyd Reyes <lloyd061302@gmail.com>
2024-01-14 13:24:53 -0700

    update

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { listNews } from '../graphql/queries';
 
-function NewsCards() {
-	return <div>News List</div>;
-}
+const NewsCards = () => {
+	const [news, setNews] = useState([]);
+
+	useEffect(() => {
+		fetchNews();
+	}, []);
+
+	const fetchNews = async () => {
+		try {
+			const newsData = await API.graphql(graphqlOperation(listNews));
+			const newsList = newsData.data.listNews.items;
+
+			console.log('news list', newsList);
+			setNews(newsList);
+		} catch (error) {
+			console.log('error on fetching news', error);
+		}
+	};
+};
 
 export default NewsCards;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Amplify.configure(amplifyconfig);
 
 root.render(
 	<React.StrictMode>
