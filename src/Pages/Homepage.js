0adcefeac5446e8f93d3a0e4bc4485c878c8db20
Lloyd Reyes <lloyd061302@gmail.com>
2024-01-15 14:41:23 -0700

    change banner to video

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -6,7 +6,7 @@ function Homepage() {
 		<div className="main-container">
 			<div className="banner">
 				<video autoPlay muted loop className="teaser">
-					<source src="/srvr_roster_teaser_3.mp4" type="video/mp4"></source>
+					<source src="https://srvr-assets.s3.us-west-1.amazonaws.com/srvr_roster_teaser_3.mp4" type="video/mp4"></source>
 				</video>
 			</div>
 			<div className="panel" id="latest-news">
@@ -20,7 +20,7 @@ function Homepage() {
 			<div className="panel" id="schedule">
 				<div className="panel-container">
 					<h1 className="panel-title">Schedule</h1>
-					<iframe allowTransparency={'true'} src={'https://view.monday.com/embed/5856431756-4344539b9f5333ce6e23f25e721cdd4c?r=use1'} width={'100%'} height={'700'} style={{ borderRadius: '10px', border: '0', boxShadow: '5px 5px 56px 0px rgba(0,0,0,0.25)', backgroundColor: 'black' }}></iframe>;
+					<iframe allowtransparency={'true'} src={'https://view.monday.com/embed/5856431756-4344539b9f5333ce6e23f25e721cdd4c?r=use1'} width={'100%'} height={'700'} style={{ borderRadius: '10px', border: '0', boxShadow: '5px 5px 56px 0px rgba(0,0,0,0.25)', backgroundColor: 'black' }}></iframe>;
 				</div>
 			</div>
 		</div>
