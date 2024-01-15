e341491a4db014ab519e7152c72710d4dd70bd76
Lloyd Reyes <lloyd061302@gmail.com>
2024-01-14 17:45:57 -0700

    update schema

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -3,6 +3,13 @@ import { generateClient } from 'aws-amplify/api';
 import * as queries from '../graphql/queries';
 
 const client = generateClient();
+const variables = {
+	filter: {
+		priority: {
+			eq: 'date',
+		},
+	},
+};
 
 function NewsCards() {
 	const [news, setNews] = useState([]);
@@ -13,15 +20,26 @@ function NewsCards() {
 
 	const fetchNews = async () => {
 		try {
-			const newsData = await client.graphql({ query: queries.listNews });
+			const newsData = await client.graphql({ query: queries.listNews, variables: variables });
 			const newsList = newsData.data.listNews.items;
-			console.log('news list', newsList);
 			setNews(newsList);
 		} catch (error) {
 			console.log('error fetching news', error);
 		}
 	};
-	return <div>NewsCards</div>;
+	return (
+		<div className="news-list">
+			{news.map((newsItem) => {
+				return (
+					<div className="news-item" key={newsItem.id}>
+						<div className="news-img" style={{ backgroundImage: `${newsItem.pathImg}` }}></div>
+						<p className="news-title">{newsItem.title}</p>
+						<p className="news-date">{newsItem.date}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default NewsCards;
