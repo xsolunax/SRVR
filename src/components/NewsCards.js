5943e3338a5b30452f70dd956c3402784e36e547
Lloyd Reyes <lloyd061302@gmail.com>
2024-03-04 18:25:27 -0700

    update news

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
-//	{ id: '2', title: '', date: '', imgPath: '' },
+//	{ id: '1', title: '', date: '', imgPath: '' },
 
 const newsList = [
-	{ id: '1', title: 'SRVR is the champion of Waypoint Lounge’s Weekly Tournament [#13] against The Farmers', date: 'January 10, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/Defeated.png' },
-	{ id: '2', title: 'SRVR ESPORTS wins semi-finals against OverHeat with a score of 13-3!', date: 'January 10, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/Defeated.png' },
-	{ id: '3', title: "We'd like to introduce you to the newest addition to the SRVR Fam, @SRVR x PERLAS!", date: 'January 10, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/SRVR-Perlas.png' },
-	{ id: '4', title: 'SRVR ESPORTS placed 2nd on the A2I Open Invitational Tournament sponsored by MSI.', date: 'January 13, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/Defeated.png' },
-	{ id: '5', title: 'WATCH SRVR ESPORTS  COMPETE IN A2i’s OPEN INVITATIONAL FINALS TOURNAMENT @ 3:45 PM EST! ', date: 'January 13, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/MatchDay.png' },
+	{ id: '1', title: 'SRVR ESPORTS will be competing in VCT Open Qualifiers 2', date: 'February 12, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/VCT_DAY_1.png' },
+	{ id: '2', title: 'SRVR ESPORTS LOST the first match up of VCT Open Qualifiers 2 against TAMU Maroon', date: 'February 12, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/Defeat_vct.png' },
+	{ id: '3', title: 'UBCEA x SFUEA x UVicEA Winter WonderLAN tournament!', date: 'February 17, 2024', imgPath: 'https://srvr-assets.s3.us-west-1.amazonaws.com/MatchDay_UBC.png' },
 ];
+
+const sortedNewsList = newsList.map((item) => item).sort((a, b) => parseInt(b.id) - parseInt(a.id));
 
 function NewsCards() {
 	const [news, setNews] = useState([]);
 
 	useEffect(() => {
-		setNews(newsList);
+		setNews(sortedNewsList);
 	}, []);
 
 	return (
